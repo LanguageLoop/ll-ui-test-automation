3e79ac4f5d0c77e98603aead262ad22b57fe2be0
LanguageLoop <developer@languageloop.com.au>
2023-01-23 17:14:15 +0530

    Added ODTI Jobs step methods

--- test/stepdefinition/ODTI/ODTIJobsSteps.js
+++ test/stepdefinition/ODTI/ODTIJobsSteps.js
@@ -116,3 +116,50 @@ Then(/^The columns available for ODTI Jobs for the user are "(.*)"$/, function (
     }
 })
 
+Then(/^The ODTI Service Charge ID, Campus Name and Interpreter Name results are not clickable and read only$/, function () {
+    browser.pause(5000)
+    let clickableTageName = "a";
+    let serviceChargeID1TextElement = $(ODTIJobsPage.odtiServiceChargeIDTextLocator.replace("<dynamic>", "1"));
+    let serviceChargeIDVisibleStatus = action.isVisibleWait(serviceChargeID1TextElement,10000);
+    let serviceChargeIDTagName = action.getElementTagName(serviceChargeID1TextElement);
+    chai.expect(serviceChargeIDVisibleStatus).to.be.true;
+    chai.expect(serviceChargeIDTagName).to.not.equal(clickableTageName);
+    let campusName1TextElement = $(ODTIJobsPage.campusNameValueTextLocator.replace("<dynamic>", "1"));
+    let campusNameVisibleStatus = action.isVisibleWait(campusName1TextElement,10000);
+    let campusNameTagName = action.getElementTagName(campusName1TextElement);
+    chai.expect(campusNameVisibleStatus).to.be.true;
+    chai.expect(campusNameTagName).to.not.equal(clickableTageName);
+    let interpreterName1TextElement = $(ODTIJobsPage.interpreterNameValueTextLocator.replace("<dynamic>", "1"));
+    let interpreterNameVisibleStatus = action.isVisibleWait(interpreterName1TextElement,10000);
+    let interpreterNameTagName = action.getElementTagName(interpreterName1TextElement);
+    chai.expect(interpreterNameVisibleStatus).to.be.true;
+    chai.expect(interpreterNameTagName).to.not.equal(clickableTageName);
+})
+
+Then(/^The No odti billings to show... message is displayed$/, function () {
+    let noBillingsMessageDisplayStatus = action.isVisibleWait(ODTIJobsPage.noBillingsMessageText,20000);
+    chai.expect(noBillingsMessageDisplayStatus).to.be.true;
+})
+
+When(/^I click on Export to Excel link$/, function () {
+    action.isClickableWait(ODTIJobsPage.exportToExcelLink,20000);
+    action.clickElement(ODTIJobsPage.exportToExcelLink);
+})
+
+Then(/^The Excel file is downloaded$/, function () {
+    browser.pause(10000);
+    let downloadsDirectoryPath = process.cwd();
+    let odtiExcelFileName;
+    let filesInDownloads = fs.readdirSync(downloadsDirectoryPath);
+    for (let i=0; i<filesInDownloads.length; i++){
+        if (filesInDownloads[i].includes("ODTIReport_")){
+            odtiExcelFileName = filesInDownloads[i];
+        }
+    }
+    let excelFilePath = process.cwd() + "/" + odtiExcelFileName;
+    let excelFileExistStatus = fs.existsSync(excelFilePath);
+    chai.expect(excelFileExistStatus).to.be.true;
+    fs.unlinkSync(excelFilePath);
+    browser.pause(5000);
+})
+
