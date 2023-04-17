21a263a75ebff2236eb924a16fa5d4d44af289d3
LanguageLoop <developer@languageloop.com.au>
2023-04-17 17:07:57 +0530

    Added new locators in Campus Details page

--- test/pages/CampusDetails/CampusDetails.js
+++ test/pages/CampusDetails/CampusDetails.js
@@ -418,4 +418,40 @@ module.exports ={
     get campusODTIPreferenceTypeCustomisedText() {
         return $('//span[text()="Gender (On-demand TI)"]/ancestor::div[@class="PreferenceInfoBlock Card"]//span[text()="CUSTOMISED"]');
     },
+
+    get addABlockLink() {
+        return $('//a[text()="Add a block"]');
+    },
+
+    get searchByContractorNameAndIdTextBox() {
+        return $('//input[contains(@id,"SearchContr")]');
+    },
+
+    get contractorResultCheckboxLocator() {
+        return '//span[contains(text(),"<dynamic>")]/parent::div/parent::div//input[@type="checkbox"]';
+    },
+
+    get contractorSeverityDropdown() {
+        return $('//select[contains(@id,"SeverityContr")]');
+    },
+
+    get startDateContractor() {
+        return $('//input[contains(@id,"StartDateContr")]');
+    },
+
+    get endDateContractor() {
+        return $('//input[contains(@id,"EndDateContr")]');
+    },
+
+    get addBlockButton() {
+        return $('//input[@value="Add Block"][@type="submit"]');
+    },
+
+    get newBlockRuleLinksToggleIcon() {
+        return $('//table[contains(@id,"Blockings")]/child::tbody/child::tr//child::label[contains(@id,"Toggle")]');
+    },
+
+    get newBlockRuleLinksRemove() {
+        return $('//table[contains(@id,"Blockings")]/child::tbody/child::tr//child::a[(text()="Remove")]');
+    },
 }
