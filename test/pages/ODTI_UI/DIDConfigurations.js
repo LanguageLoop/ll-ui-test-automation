9afe74f5a167b99831c4ece85ecfeaa093fd51ff
LanguageLoop <developer@languageloop.com.au>
2023-04-26 13:18:51 +0530

    Added locators in DID Configurations page

--- test/pages/ODTI_UI/DIDConfigurations.js
+++ test/pages/ODTI_UI/DIDConfigurations.js
@@ -7,5 +7,13 @@ module.exports = {
 
     get newConfigurationButtonUnderCampusConfiguration() {
         return $('//input[@value="New Configuration" and contains(@onclick,"Campus")]');
+    },
+
+    get editCampusConfigurationLinkLocator() {
+        return '(//a[text()="<dynamicCampusPin>"]/parent::td/following-sibling::td/a[text()="Edit"])[<dynamicIndex>]';
+    },
+
+    get editCampusConfigurationLinksLocator() {
+        return '//a[text()="<dynamicCampusPin>"]/parent::td/following-sibling::td/a[text()="Edit"]';
     }
 }
