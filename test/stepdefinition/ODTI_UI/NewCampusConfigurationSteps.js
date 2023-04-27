9c4202e32fad4f42758aa48c1b6911b70d133405
LanguageLoop <developer@languageloop.com.au>
2023-04-27 10:39:06 +0530

    Added steps in New campus configuration page

--- test/stepdefinition/ODTI_UI/NewCampusConfigurationSteps.js
+++ test/stepdefinition/ODTI_UI/NewCampusConfigurationSteps.js
@@ -14,3 +14,20 @@ Then(/^the Campus PIN input and configuration section are displayed$/, function 
     let configurationSectionDisplayStatus = action.isVisibleWait(newCampusConfigurationPage.configurationSection, 10000);
     chai.expect(configurationSectionDisplayStatus).to.be.true;
 })
+
+Then(/^the saved configuration is displayed upon clicking duplicate$/, function () {
+    let savedConfigurationDisplayStatus = action.isVisibleWait(newCampusConfigurationPage.savedConfiguration, 10000);
+    chai.expect(savedConfigurationDisplayStatus).to.be.true;
+})
+
+Then(/^the Campus PIN input and Service Type Dropdown are displayed upon clicking duplicate$/, function () {
+    let campusPinInputDisplayStatus = action.isVisibleWait(newCampusConfigurationPage.campusPinInputTextBox, 10000);
+    chai.expect(campusPinInputDisplayStatus).to.be.true;
+    let TIServiceTypeDropdownDisplayStatus = action.isVisibleWait(newCampusConfigurationPage.TIServiceTypeDropdownDisabled, 10000);
+    chai.expect(TIServiceTypeDropdownDisplayStatus).to.be.true;
+})
+
+Then(/^the Campus PIN is blank upon clicking duplicate$/, function () {
+    let campusPinInputElementHTML = action.getElementHTML(newCampusConfigurationPage.campusPinInputTextBox);
+    chai.expect(campusPinInputElementHTML).to.not.includes("value");
+})
