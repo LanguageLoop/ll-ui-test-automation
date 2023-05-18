9c0304b883e9b36f9e8a0603060e32ed84a92f6d
LanguageLoop <developer@languageloop.com.au>
2023-05-18 10:37:06 +0530

    Added methods in New DID configuration steps

--- test/stepdefinition/ODTI_UI/NewDIDConfigurationSteps.js
+++ test/stepdefinition/ODTI_UI/NewDIDConfigurationSteps.js
@@ -8,3 +8,14 @@ When(/^a blank form is shown in New DID Configuration$/, function () {
     let didNumberOrigValue = action.getElementAttribute(newDIDConfigurationPage.didNumberInputTextBox,"origvalue");
     chai.expect(didNumberOrigValue).to.equal("");
 })
+
+When(/^a copy of the existing DID configuration is shown upon clicking duplicate$/, function () {
+    let existingDIDConfigurationDisplayStatus = action.isVisibleWait(newDIDConfigurationPage.existingDIDConfiguration,10000);
+    chai.expect(existingDIDConfigurationDisplayStatus).to.be.true;
+})
+
+When(/^the DID is blank in existing DID configuration upon clicking duplicate$/, function () {
+    action.isVisibleWait(newDIDConfigurationPage.didNumberInputTextBox,10000);
+    let didNumberOrigValue = action.getElementAttribute(newDIDConfigurationPage.didNumberInputTextBox,"origvalue");
+    chai.expect(didNumberOrigValue).to.equal("");
+})
