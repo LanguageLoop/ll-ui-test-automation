4ca36b7c0e6fd58addc652f8517862de9c69b635
LanguageLoop <developer@languageloop.com.au>
2023-05-23 11:41:03 +0530

    Added new locators in DID configuration page

--- test/pages/ODTI_UI/NewDIDConfiguration.js
+++ test/pages/ODTI_UI/NewDIDConfiguration.js
@@ -8,4 +8,24 @@ module.exports = {
     get existingDIDConfiguration() {
         return $('//div[contains(@id,"DIDConfigurationForm")]');
     },
+
+    get tiServiceTypeDropdown() {
+        return $('//select[contains(@id,"DIDConfiguration_TIServiceType")]')
+    },
+
+    get campusPinInputTextBox() {
+        return $('//input[contains(@id,"DIDConfiguration_CampusBillToId")]');
+    },
+
+    get campusNameBelowInputTextBox() {
+        return $('//span[contains(@id,"IsWithCampus")]/a');
+    },
+
+    get milsConfigurationSection() {
+        return $('//div[contains(text(),"MILS") and (contains(text(),"Configuration"))]/parent::div');
+    },
+
+    get tixpConfigurationSection() {
+        return $('//div[contains(text(),"TIXP") and (contains(text(),"Configuration"))]/parent::div');
+    },
 }
