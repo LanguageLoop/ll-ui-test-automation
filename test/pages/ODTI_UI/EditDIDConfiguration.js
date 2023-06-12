785fbbba9aaffb7a768ea11e3c36a8da19782904
LanguageLoop <developer@languageloop.com.au>
2023-06-12 16:32:03 +0530

    Added locators in Edit DID configuration

--- test/pages/ODTI_UI/EditDIDConfiguration.js
+++ test/pages/ODTI_UI/EditDIDConfiguration.js
@@ -4,4 +4,16 @@ module.exports = {
     get existingConfiguration() {
         return $('//div[contains(@id,"DIDConfigurationForm")]');
     },
+
+    get xIconBesideLanguageOptionDynamicLocator() {
+        return '//table[contains(@id,"TableLanguage")]/tbody/tr[<dynamic>]/td[4]//span';
+    },
+
+    get languageTextDynamicLocator() {
+        return '//table[contains(@id,"TableLanguage")]/tbody/tr[<dynamic>]/td[2]/div';
+    },
+
+    get languageTableBodyRowsCount() {
+        return $$('//table[contains(@id,"TableLanguage")]/tbody/tr').length;
+    },
 }
