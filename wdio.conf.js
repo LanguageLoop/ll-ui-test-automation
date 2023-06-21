6420df6bc7b585915ef102da54ebb867d5f573d0
LanguageLoop <developer@languageloop.com.au>
2023-06-21 10:53:35 +0530

    Enabled all specs in config

--- wdio.conf.js
+++ wdio.conf.js
@@ -96,8 +96,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './test/features/**/*.feature'
-        './test/features/InterpretingBookingManagement/InterpreterStatus.feature'
+        './test/features/**/*.feature'
         // './test/features/**/Claims.feature'
     ],
     // Patterns to exclude.
