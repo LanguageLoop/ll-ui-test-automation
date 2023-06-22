b93e667ec459f50f0972305255e0126421cf6a68
LanguageLoop <developer@languageloop.com.au>
2023-06-22 17:03:02 +0530

    Added locators in ODTI Job details page

--- test/pages/ODTI/ODTIJobDetails.js
+++ test/pages/ODTI/ODTIJobDetails.js
@@ -11,5 +11,21 @@ module.exports = {
 
     get newPinMustBeSelectedAndValidErrorMessage() {
         return $('//div[text()="New PIN must be selected and valid"]');
+    },
+
+    get selectPinLink() {
+        return $('//a[text()="Select PIN"]');
+    },
+
+    get pinTextBoxUnderFilterBy() {
+        return $('input[placeholder="PIN"]')
+    },
+
+    get pinSearchResultLinkDynamicLocator() {
+        return '//a[contains(@id,"ChangeJobPINModal") and text()="<dynamic>"]';
+    },
+
+    get noContractRateFoundErrorMessage() {
+        return $('//div[text()="The Job PIN could not be migrated. No contract rate found for Contract ID:1136 and will not allow the user to proceed with that PIN."]');
     }
 }
