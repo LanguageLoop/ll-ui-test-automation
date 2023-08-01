7039b9aed1f34d7ca52b834cc64b2024adf1da45
LanguageLoop <developer@languageloop.com.au>
2023-08-01 17:32:31 +0530

    Added locators in ODTI contractors page

---
+++ test/pages/AdminTools/ODTIContractorsPage.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+
+    get showLoggedInContractorsOnlyToggleInput() {
+        return $('//div[text()="Show Logged in Contractors Only"]/parent::div//div[contains(@class,"ToggleButton")]//input');
+    },
+
+    get showLoggedInContractorsOnlyToggleLabel() {
+        return $('//div[text()="Show Logged in Contractors Only"]/parent::div//div[contains(@class,"ToggleButton")]/label');
+    },
+
+    get searchByIdAndNameTextBox() {
+        return $('//input[@placeholder="Search by Id and Name"]');
+    },
+
+    get searchButton() {
+        return $('//input[@value="Search"]');
+    },
+
+    get isLoggedOnToggleInput() {
+        return '//a[contains(text(),"<dynamic>")]/parent::td/parent::tr/child::td[3]//div[contains(@class,"ToggleButton")]//input';
+    },
+
+    get isLoggedOnToggleLabel() {
+        return '//a[contains(text(),"<dynamic>")]/parent::td/parent::tr/child::td[3]//div[contains(@class,"ToggleButton")]/label';
+    },
+}
