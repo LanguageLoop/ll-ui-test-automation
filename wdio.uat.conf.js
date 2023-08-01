7f99ff7578ded01bb6b387ada4f656c1f532d870
LanguageLoop <developer@languageloop.com.au>
2023-08-01 17:34:11 +0530

    Added global URL variable

--- wdio.uat.conf.js
+++ wdio.uat.conf.js
@@ -4,6 +4,7 @@ const GlobalData=require('./test/data/GlobalData')
 GlobalData.BASE_URL="https://li-uat.languageloop.com.au/LoopedIn_th/Login.aspx"
 GlobalData.DEV_URL="https://li-uat.languageloop.com.au/DeveloperScreen/Home.aspx"
 GlobalData.ODTI_DID_URL="https://li-uat.languageloop.com.au/OnDemandTI_UI/"
+GlobalData.ADMIN_TOOLS_URL="https://li-uat.languageloop.com.au/AdminDataTools/"
 
 
 const uatconfig = Object.assign(baseConfig, {
