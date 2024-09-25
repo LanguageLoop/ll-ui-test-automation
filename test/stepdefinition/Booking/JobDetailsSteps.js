5d82d573cbe166c1a9f03f319327ae27fae425db
LanguageLoop <developer@languageloop.com.au>
2024-09-25 17:37:01 +0530

    Updated the confirmation popup locator

--- test/stepdefinition/Booking/JobDetailsSteps.js
+++ test/stepdefinition/Booking/JobDetailsSteps.js
@@ -113,7 +113,8 @@ When(/^I set the contractor job status from "(.*)" to "(.*)"$/, function(origina
     action.isVisibleWait(confirmationWindow,30000,"Confirmation window in Job Details page")
     if(confirmationWindow.isDisplayed())
     {
-        const confirmYes =$('//input[contains(@id,"wtActions_wt145")]')
+        //const confirmYes =$('//input[contains(@id,"wtActions_wt145")]')
+        const confirmYes = $('//input[contains(@id,"wtOverlapConfirmationModal") and (@value="Yes")]');
         action.isClickableWait(confirmYes,30000,"Confirm Yes button in Job Details page")
         action.clickElement(confirmYes,"Confirm Yes button in Job Details page")
     }
