c82702a3427077e25359f8d8697cd434c9b99b58
LanguageLoop <developer@languageloop.com.au>
2025-02-19 17:31:18 +0530

    Updated label from Contractor to Interpreter

--- test/pages/Claims/ClaimsPage.js
+++ test/pages/Claims/ClaimsPage.js
@@ -38,7 +38,7 @@ module.exports ={
     },
 
     get contractorFeeInput(){
-        return $('//label[text()="Contractor Payment"]/..//input')
+        return $('//label[text()="Interpreter Payment"]/..//input')
     },
 
     get rscFeeInput(){
@@ -54,19 +54,19 @@ module.exports ={
     },
 
     get processContractorButton(){
-        return $('//input[@value="Process Contractor"]')
+        return $('//input[@value="Process Interpreter"]')
     },
 
     get reprocessContractorButton(){
-        return $('//input[@value="Reprocess Contractor"]')
+        return $('//input[@value="Reprocess Interpreter"]')
     },
 
     get processCampusAndContractorButton(){
-        return $('//input[@value="Process Campus and Contractor"]')
+        return $('//input[@value="Process Campus and Interpreter"]')
     },
 
     get reprocessCampusAndContractorButton(){
-        return $('//input[@value="Reprocess Campus and Contractor"]')
+        return $('//input[@value="Reprocess Campus and Interpreter"]')
     },
 
     get jobStatusColumn(){
@@ -81,7 +81,7 @@ module.exports ={
         return $('//*[text()="Reprocess Campus"]/../..//input[@value="Yes"]')
     },
     get reprocessCampusAndContractorConfirmYesButtonNew(){
-        return $('//*[text()="Reprocess Campus and Contractor"]/../..//input[@value="Yes"]')
+        return $('//*[text()="Reprocess Campus and Interpreter"]/../..//input[@value="Yes"]')
     },
     
 
